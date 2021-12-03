ad05a37abb38d141658e4024c0569f88f7878e02
Alan Delimon <adelimon@gmail.com>
2021-12-02 22:34:55 -0500

    Cleanup on this package - remove old files and vuln cleanup.

--- src/Renew.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Renew from './Renew';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Renew />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
